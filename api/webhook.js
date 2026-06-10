b30a5df59539d3a83d94ebfdee835162eb0d81e0
Askar64 <askarkapashev30@gmail.com>
2026-06-10 18:18:45 +0500

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -76,6 +76,11 @@ export default async function handler(req, res) {
     if (!message?.text) return res.status(200).json({ ok: true });
 
     const chatId = message.chat.id;
+    console.log('====================');
+    console.log('CHAT ID:', chatId);
+    console.log('USERNAME:', message.from?.username);
+    console.log('FIRST NAME:', message.from?.first_name);
+    console.log('====================');
     const text = message.text;
 
     // Команда /start
